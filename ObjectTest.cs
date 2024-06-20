3d57f37264e6b1f84a1b82a5e83f59acd26f3874
Nicolas de Souza <nicolas.souza@radixeng.com.br>
2024-06-20 16:23:24 -0300

    POC

---
+++ ObjectTest.cs
@@ -0,0 +1,8 @@
+namespace UpdateObjectByProperties
+{
+    public class ObjectTest
+    {
+        public int ID { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using UpdateObjectByProperties;
+
+var oldObj = new ObjectTest(){ID = 10, MyProperty = "Teste 01"};
+var newObj = new ObjectTest(){ID = 10, MyProperty = "Teste 02"};
+
+System.Console.WriteLine("Old value: " + oldObj.MyProperty);
+
+UpdateObject(oldObj, newObj);
+
+System.Console.WriteLine("New value: " + oldObj.MyProperty);
+
+void UpdateObject<ObjectTest>(ObjectTest oldOs, ObjectTest os)
+{
+    PropertyInfo[] property = typeof(ObjectTest).GetProperties();
+
+    foreach (PropertyInfo propriedade in property)
+    {
+        var oldValue = propriedade.GetValue(oldOs);
+        var newValue = propriedade.GetValue(os);
+
+        if (oldValue != null && !oldValue.Equals(newValue))
+        {
+            propriedade.SetValue(oldOs, newValue);
+        }
+        else if (oldValue == null && newValue != null)
+        {
+            propriedade.SetValue(oldOs, newValue);
+        }
+    }
+}
